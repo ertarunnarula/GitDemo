85df523ad90c228c44ef7451f4db397f163afef5
tarun <ertarunnarula@gmail.com>
2021-02-07 16:10:51 +0530

    Second Commit

---
+++ src/test/java/pages/GitExample.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.testng.annotations.Test;
+
+public class GitExample {
+	
+	@Test
+	public void postJira() {
+		System.out.println("this is postJira 1");
+		System.out.println("this is postJira 2");
+		System.out.println("this is postJira 3");
+		System.out.println("this is postJira 4");
+		System.out.println("this is postJira 5");
+	}
+
+}
