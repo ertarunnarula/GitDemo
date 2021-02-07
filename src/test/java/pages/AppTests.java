08846422b593357f2af80ac1c462fa1a2383df50
tarun <ertarunnarula@gmail.com>
2021-02-07 20:51:32 +0530

    Third Commit

---
+++ src/test/java/pages/AppTests.java
@@ -0,0 +1,27 @@
+package pages;
+
+import org.testng.annotations.Test;
+
+public class AppTests {
+	
+	@Test
+	public void iOSTest() {
+		System.out.println("iOS test 1");
+		System.out.println("iOS test 2");
+		System.out.println("iOS test 3");
+		System.out.println("iOS test 4");
+		System.out.println("iOS test 5");
+		System.out.println("iOS test 6");
+	}
+	
+	@Test
+	public void AndroidTest() {
+		System.out.println("Android test 1");
+		System.out.println("Android test 2");
+		System.out.println("Android test 3");
+		System.out.println("Android test 4");
+		System.out.println("Android test 5");
+		System.out.println("Android test 6");
+	}
+
+}
